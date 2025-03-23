9f1a6ada0dfb652d2fb0e1f813f2952040545cf5
Stranger133 <stevenzaayter13@gmail.com>
2025-03-23 19:07:18 +0200

    navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/NavBar';
+import Menu from './components/Menu';
+import About from './components/About';
+
+function App() {
+
+  return (
+    <div className="App">
+      <NavBar/>
+      <div className='body'>
+        <Routes>
+          <Route path='Menu' element={<Menu/>}></Route>
+          <Route path='About' element={<About/>}></Route>
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import Icon from './Icon'
+import { Link } from 'react-router-dom'
+import './css/NavBar.css';
+
+export default function NavBar() {
+    const [user, setUser] = useState("User");
+
+    useEffect(()=> setUser("User"), [])
+  return (
+    <div className='nav'>
+        <div className='logo'>
+            <Icon/>
+        </div>
+        <div className='list'>
+            <span>
+                <Link className='link' to='Menu'>Menu</Link>
+            </span>
+            <span>
+                <Link className='link' to='About'>About</Link>
+            </span>
+            <span>
+                <p>Hi, {user}</p>
+            </span>
+        </div>
+    </div>
+  )
+}
